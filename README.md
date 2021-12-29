5293dfb750986d311145e48f3dc1248eb9c2737e
Remu Ollinen <remu.ollinen@gmail.com>
2021-12-29 16:25:07 +0200

    find buy-sell days, added styling and updated readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Pre-assignment for Vincit Rising Star trainee program
+
+- A small bitoin application made with React
+- Cryptocurrency data fetched from [CoinGecko](http://coingecko.com/en/api) API
+
+## Functionalities
+
+- Calculate longest bearish (downward) trend in given date range
+- Find trading day with the highest volume in given date range and the volume in euros
+- Find the best days to buy and sell bitcoin in a given date range
+
+## Resources used
+
+- Installed package _react-numeral_ to format large numbers (i.e. highest volume in euros)
+- This [Medium Article](https://enlear.academy/leetcode-algorithm-challenges-best-time-to-buy-and-sell-stock-4114caffb5e7) helped me undestand better how to find the best buying and selling days in a given range
+
+## Screenshot
+
+![screenshot of application](screenshot.png)
--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default class App extends Component {
 		bearish: "",
 		volumeData: [],
 		highestDay: "",
+		buyAndSell: [],
 	};
 
 	getBitcoinData = (e) => {
@@ -21,19 +22,25 @@ export default class App extends Component {
 			.then((res) => res.json())
 			.then((data) => {
 				// longest bearish trend
-				let differenceInDays = (this.state.to - this.state.from) / 3600 / 24;
+				let differenceInDays = (this.state.to - this.state.from) / 3600 / 24; // difference between given inputs
 				let priceArray = [];
+				let dayAndPriceArray = [];
 				data.prices.forEach((item) => {
 					if (differenceInDays <= 90 && new Date(item[0]).getHours() === 0) {
 						priceArray.push(item[1]);
+						dayAndPriceArray.push({ day: item[0], price: item[1] });
 					} else if (differenceInDays > 90) {
 						priceArray.push(item[1]);
+						dayAndPriceArray.push({ day: item[0], price: item[1] });
 					}
 				});
 				this.calculateBearishTrend(priceArray);
 
 				// highest volume
 				this.getHighestVolumeDay(data.total_volumes);
+
+				// buy and sell dates (time machine)
+				this.maximizeProfits(dayAndPriceArray);
 			});
 	};
 
@@ -44,7 +51,6 @@ export default class App extends Component {
 		for (let i = 1; i < array.length; i++) {
 			if (array[i] < array[i - 1]) {
 				stack.push(array[i]);
-				// console.log(stack, i);
 				if (record.length === 0 || record.length < stack.length) {
 					record = stack;
 				}
@@ -53,8 +59,6 @@ export default class App extends Component {
 				stack.push(array[i]);
 			}
 		}
-		// console.log(array);
-		// console.log(record);
 		this.setState({
 			bearish: record.length - 1,
 		});
@@ -62,12 +66,46 @@ export default class App extends Component {
 
 	getHighestVolumeDay = (array) => {
 		let sortedData = array.sort((a, b) => b[1] - a[1]); // sorts the data in desc order
-		// console.log(sortedData[0]);
-		let highestVolumeDay = new Date(sortedData[1][0]); // converts date from UNIX to readable date
+		let highestVolumeDay = new Date(sortedData[1][0]).toDateString(); // converts date from UNIX to readable date
 		this.setState({
 			volumeData: sortedData[0], // sets highest volume data (includes timestamp and volume)
 			highestDay: highestVolumeDay, // sets the date of the highest volume
 		});
+	};
+
+	maximizeProfits = (array) => {
+		let buyDate;
+		let sellDate;
+		// store a pair of days
+		let dates = [];
+		// store maximum profit
+		let maxProfit = 0;
+		for (let i = 0; i < array.length; i++) {
+			let buyingPrice = array[i].price;
+			for (let j = i + 1; j < array.length; j++) {
+				let sellingPrice = array[j].price;
+				if (sellingPrice > buyingPrice) {
+					let profit = sellingPrice - buyingPrice;
+					if (profit > maxProfit) {
+						maxProfit = profit;
+						dates = [];
+						// convert UNIX timestamps to readable dates
+						buyDate = new Date(array[i].day).toDateString();
+						sellDate = new Date(array[j].day).toDateString();
+						dates.push(buyDate, sellDate);
+					}
+				}
+			}
+		}
+		if (dates.length > 0) {
+			this.setState({
+				buyAndSell: dates,
+			});
+		} else {
+			this.setState({
+				buyAndSell: ["Don't buy", "Or sell"],
+			});
+		}
 	};
 
 	dateHandler = (e) => {
@@ -86,8 +124,12 @@ export default class App extends Component {
 	render() {
 		return (
 			<div className="app">
+				<h1> Help Scrooge McDuck to invest in Bitcoin!</h1>
+				<p>
+					Select your date range from the inputs below and get analytics from
+					the market data.
+				</p>
 				<form>
-					<h2>Select a date range</h2>
 					<label htmlFor="fromDate">
 						From:{" "}
 						<input
@@ -110,29 +152,52 @@ export default class App extends Component {
 						Get data
 					</button>
 				</form>
-				<div>
-					<h3>
-						The longest bearish trend: <span>{this.state.bearish || "X"}</span>{" "}
-						days
-					</h3>
-					<h3>
-						The day with the highest volume:{" "}
-						<span>
-							{this.state.highestDay.toLocaleString("en-GB", {
-								weekday: "long",
-								day: "numeric",
-								month: "long",
-								year: "numeric",
-							})}
-						</span>
-					</h3>
-					<h3>
-						Volume on that day in euros (€):
-						<span>
-							{<Numeral value={this.state.volumeData[1]} format={"(0,00)"} />} €
-						</span>
-					</h3>
+				<div className="result-container">
+					<div className="result-card">
+						<h2>Longest bearish trend</h2>
+						<div className="result">
+							<h3>Days in a row</h3>
+							<span>{this.state.bearish} days</span>
+						</div>
+					</div>
+					<div className="result-card">
+						<h2>Highest volume</h2>
+						<div className="result">
+							<h3>Highest Volume Day</h3>
+							<span>{this.state.highestDay || "-"}</span>
+							<h3>Volume in euros (€)</h3>
+							<span>
+								{
+									<Numeral
+										value={this.state.volumeData?.[1]}
+										format={"(0,00)"}
+									/>
+								}
+								€
+							</span>
+						</div>
+					</div>
+					<div className="result-card">
+						<h2>Buy & Sell</h2>
+						<div className="result">
+							<h3>Buy</h3>
+							<span>
+								{this.state.buyAndSell.length > 0
+									? this.state.buyAndSell[0]
+									: "-"}
+							</span>
+							<h3>Sell</h3>
+							<span>
+								{this.state.buyAndSell.length > 0
+									? this.state.buyAndSell[1]
+									: "-"}
+							</span>
+						</div>
+					</div>
 				</div>
+				<footer>
+					<p>Copyright &copy; Remu Ollinen 2021</p>
+				</footer>
 			</div>
 		);
 	}
